cc9f98389df66680def65e6a306ac36807a11743
William Parsons <parwi096@student.otago.ac.nz>
2023-10-14 12:41:06 +1000

    saving and loading

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,6 +1,9 @@
-class Cart:
-    # TODO: Implement the cart class
+import json
+import os
 
+class Cart:
+    print()
+    # TODO: Implement the cart class
 
 class Item:
     def __init__(self, ID, Name, Cost):
@@ -16,16 +19,73 @@ Catalogue = [
     Item(5, "Couch", 1000.00)
 ]
 
-def load_cart():
-    # TODO: Implement loading the cart from ./data.json. If the file is missing, return an empty cart.
+def getID(name):
+    """
+    Utility method to get the ID of an item, given the name.
+    """
+
+    for item in Catalogue:
+        if item.Name == name:
+            return item.ID
+    return -1
+
+def getName(id):
+    """
+    Utility method to get the name of an item, given the ID.
+    """
+
+    for item in Catalogue:
+        if item.ID == id:
+            return item.Name
     return None
 
+def debugCart(cart):
+    """
+    Utility method to print the current contents of the cart.
+    """
+        
+    for k, v in cart.items():
+        print(k, getName(k), v)
+
+def load_cart():
+    """
+    Loads cart data from 'data.json' and returns it as a Dictionary where
+    the key is the item ID, and the value is the item quantity
+    """
+
+    path = 'data.json'
+    if not os.path.exists(path):
+        return
+    file = open(path)
+    data = json.load(file)
+
+    result = {}
+    for item in data:
+        result[int(item['id'])] = int(item['quantity'])
+
+    return result
+
 def save_cart(cart):
-    # TODO: Implement save a cart to ./data.json.
-    return None
+    """
+    Saves the contents of the cart to data.json
+    """
+
+    items = []
+    for k, v in cart.items():
+        items.append(
+            {
+                "id": k,
+                "quantity": v
+            }
+        )
+    obj = json.dumps(items, indent=4)
+
+    with open("data.json", "w") as outfile:
+        outfile.write(obj)
 
 def main():
     cart = load_cart()
+    save_cart(cart)
 
 if __name__ == "__main__":
     main()
